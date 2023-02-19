0ab4e5bffb5ae63405468facb675d29758a44c06
husk <huskgit@gmail.com>
2023-02-19 17:06:02 +0900

    chore : 오타 수정 + 여우 펫 프리팹

--- Aster_Game/Assets/Script/Character/Interfaces/IDamageable.cs
+++ Aster_Game/Assets/Script/Character/Interfaces/IDamageable.cs
@@ -3,7 +3,7 @@ namespace AG.PlayerComponent.Interfaces
     public interface IDamageable
     {
         /// <summary> 데미지 추가 </summary>
-        bool ApplyDagame(int damage);
+        bool ApplyDamage(int damage);
         /// <summary> ApplyDamage후 체력이 0 이하가 되면 이 함수 호출 </summary>
         void Die();
         /// <summary> 플레이어가 현재 죽었는지를 반환 </summary>
